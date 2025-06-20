c95c1404319e781d75bd4ac85c87c22afe57b323
Andy Huynh <andy4thehuynh@gmail.com>
2025-06-20 04:23:01 -0600

    Remove codecompanion

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -12,15 +12,6 @@ map.set('n', '\\=', ':tabnew<CR>', { noremap = true, silent = true })
 map.set('n', '\\-', ':split<CR>', { noremap = true, silent = true })
 map.set('n', '\\\\', ':vsplit<CR>', { noremap = true, silent = true })
 
-map.set('n', '<leader>a', '<cmd>CodeCompanionActions<CR>', { desc = 'Open CodeCompanion actions' })
-map.set('v', '<leader>a', '<cmd>CodeCompanionActions<CR>', { desc = 'Open CodeCompanion actions' })
-
-map.set('n', '<leader>at', '<cmd>CodeCompanionChat Toggle<CR>', { desc = 'Open CodeCompanion chat' })
-map.set('v', '<leader>at', '<cmd>CodeCompanionChat Toggle<CR>', { desc = 'Open CodeCompanion chat' })
-
-map.set('n', '<leader>ai', '<cmd>CodeCompanion<CR>', { desc = 'Open CodeCompanion inline' })
-map.set('v', '<leader>ai', '<cmd>CodeCompanion<CR>', { desc = 'Open CodeCompanion inline' })
-
 map.set('n', '<leader>g', '<cmd>G<CR>', { desc = 'Open [G]it', noremap = true })
 
 return {}
